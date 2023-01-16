6c4379c47e9bb7ae7705bfa4f3f6fad65314a16c
Evgeny <eugenesvk@users.noreply.github.com>
2023-01-16 07:43:57 +0000

    Update oculante from 0.6.41 to 0.6.50

--- Casks/oculante.rb
+++ Casks/oculante.rb
@@ -1,6 +1,6 @@
 cask "oculante" do
-  version "0.6.41"
-  sha256 "2448eb0eb98fc70d1bfdf4dc3a4b5b8e21116315fba3cc195379b0421d06a4bf"
+  version "0.6.50"
+  sha256 "959746fe307e0b23d67e4d9cf85e7d7a3030efeebe76299b0a863924c455d5cc"
 
   url "https://github.com/woelper/oculante/releases/download/#{version}/oculante_mac.zip"
   name "oculante"
