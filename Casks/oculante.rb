4251f494c4ed16f77970f17586e5ea920e7c7d08
eugenesvk <eugenesvk@users.noreply.github.com>
2025-07-21 01:35:53 +0700

    update cask schema and bump oculante to 0.9.2

--- Casks/oculante.rb
+++ Casks/oculante.rb
@@ -1,8 +1,13 @@
 cask "oculante" do
-  version "0.9.1"
-  sha256 "f16c8464805e79bc814997040ee19bb4a26c2248f74d47f19fa332da14e68819"
+  arch arm: "silicon", intel: "universal"
 
-  url "https://github.com/woelper/oculante/releases/download/#{version}/oculante_mac.zip"
+  version "0.9.2"
+  sha256 arm:          "c3a59da9254c4b0912662c8fa6f0670551500481ee904270602149a33fdbd292",
+         intel:        "c17742e6ebb28eb27f4696e43f5cb1446ac5636be83f051a2ddd60817500f698",
+         arm64_linux:  "0",
+         x86_64_linux: "0"
+
+  url "https://github.com/woelper/oculante/releases/download/#{version}/oculante_mac_#{arch}.zip"
   name "oculante"
   desc "Minimalistic crossplatform image viewer written in Rust"
   homepage "https://github.com/woelper/oculante"
